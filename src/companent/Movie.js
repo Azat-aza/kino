d4c5b81458fc5d6acdd6022a83308229e2beddb3
Graza <709507028aizada@gmail.com>
2022-09-27 14:04:51 +0300

    kino

---
+++ src/companent/Movie.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+const IMG_URL = 'https://image.tmdb.org/t/p/w1280'
+function Movie({ movie }) {
+    return (
+        <div class="card">
+            <img className="image" src={IMG_URL + movie.poster_path} />
+            <div className="info">
+                <h1 className="name">{movie.name || movie.title}</h1>
+                <span className="span">{movie.overview}</span>
+            </div>
+
+        </div>
+
+    )
+}
+export default Movie
+
+
+
